18ce718ac41f3fbfd8c8d646e48023e20cf2085a
vivekhegde2000 <125129028+vivekhegde2000@users.noreply.github.com>
2024-12-18 20:28:22 +0530

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/vivekhegde2000/otp-generator",
+  base: "/otp-generator",
   plugins: [react()],
 })
